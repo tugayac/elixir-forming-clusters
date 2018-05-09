5a62cd58003704eb738b7d89c0c2d684d63aba2b
Arda Tugay <ardactugay@gmail.com>
2018-05-09 17:58:32 -0400

    Docker-compose and Dockerfile initialization

---
+++ lib/forming_clusters.ex
@@ -0,0 +1,18 @@
+defmodule FormingClusters do
+  @moduledoc """
+  Documentation for FormingClusters.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FormingClusters.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/forming_clusters_test.exs
@@ -0,0 +1,8 @@
+defmodule FormingClustersTest do
+  use ExUnit.Case
+  doctest FormingClusters
+
+  test "greets the world" do
+    assert FormingClusters.hello() == :world
+  end
+end
